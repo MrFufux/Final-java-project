c923c373da7f4e85ad1833c821f25d52c71a6b22
Andres Felipe Fuentes Atencia <andres.fuentes@globant.com>
2021-10-14 12:45:50 -0500

    Created the menu in the main Class and the switch for the option chosen by the user and move the arrayList to the University Class

--- src/Data/University.java
+++ src/Data/University.java
@@ -1,4 +1,17 @@
 package Data;
 
+import Data.Course.Course;
+import Data.Student.Student;
+import Data.Teachers.Teacher;
+
+import java.util.ArrayList;
+
 public class University {
+
+    //Lists
+    ArrayList<Student> studentList = new ArrayList<>();
+    ArrayList<Teacher>teacherList = new ArrayList<>();
+    ArrayList<Course>courseList = new ArrayList<>();
+
+
 }
--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,18 +1,48 @@
 package Test;
 
-import Data.Course.Course;
-import Data.Student.Student;
-import Data.Teachers.Teacher;
 
-import java.util.ArrayList;
+import Data.University;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        University university = new University();
+        int selection;
 
-        //Lists
-        ArrayList<Student>studentList = new ArrayList<>();
-        ArrayList<Teacher>teacherList = new ArrayList<>();
-        ArrayList<Course>courseList = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\nHello User! Welcome to the Manitoba Campus of the Awesome University. Interface.\n\n" +
+                "PLEASE CHOOSE AN OPTION TO PROCEED: \n" +
+                "(1) Teacher List.\n" +
+                "(2) All the courses at the campus.\n" +
+                "(3) Courses List" +
+                "(4) Register a new Student.\n" +
+                "(5) Register a new Course.\n" +
+                "(6) Exit");
+
+        selection = scan.nextInt();
+
+        switch (selection){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                break;
+        }
+
+
+
+
 
     }
 }
